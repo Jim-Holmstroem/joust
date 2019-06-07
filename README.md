db3741bcad31882861a55d9021846c66e0a892d9
Jim Holmstrom <jim.holmstroem@gmail.com>
2019-06-07 19:18:36 +0000

    does not read

--- README.md
+++
@@ -1,4 +0,0 @@
-p1 250 (after input has been sent)
-p2 250
-p3 250
-p4 250
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::process::{
     Command,
+    Child,
     Stdio,
 };
 use std::{
@@ -44,11 +45,63 @@ fn run<S: AsRef<ffi::OsStr> + ?Sized>(
     ))
 }
 
+struct Program {
+    reader: Box<dyn BufRead>,
+    writer: Box<dyn Write>,
+}
+
+impl Program {
+    fn new(command_str: String) -> io::Result<Program> {
+        let command = Command::new(command_str)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()?;
+
+        Ok(Program {
+            reader: Box::new(
+                BufReader::new(
+                    command
+                        .stdout
+                        .ok_or_else(|| {
+                            io::Error::new(
+                                io::ErrorKind::Other,
+                                "Could not capture stdout"
+                            )
+                        })?,
+                )
+            ),
+            writer: Box::new(
+                BufWriter::new(
+                    command
+                        .stdin
+                        .ok_or_else(|| {
+                            io::Error::new(
+                                io::ErrorKind::Other,
+                                "Could not capture stdin"
+                            )
+                        })?,
+                )
+            ),
+        })
+    }
+
+    fn act(&mut self, input: String) -> io::Result<String> {
+        self.writer.write_all(input.as_bytes());
+
+        let mut output = String::new();
+        self.reader.read_line(&mut output);
+
+        Ok(output)
+    }
+}
+
 fn main() -> Result<(), io::Error> {
-    let reader = run("./scripts/a.sh");
-    reader?
-        .for_each(|line| println!("{}", line));
+    let mut program = Program::new("./scripts/a.sh".to_string())?;
+
+    loop {
+        println!(".");
+        program.act("something new\n".to_string());
+    }
 
     Ok(())
-
 }
